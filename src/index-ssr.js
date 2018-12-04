06ae5aa6ca0c8d4e767258d1c29156f3668f2024
孙亮 <sunliang@qudian.com>
2018-12-04 21:03:50 +0800

    fix:reposition

--- src/index-ssr.js
+++ src/index-ssr.js
@@ -1,4 +1,4 @@
 import React from 'react'
-import App from './vues/index.jsx' // eslint-disable-line
+import App from './views/index.jsx' // eslint-disable-line
 
 export default <App /> // eslint-disable-line
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './vues/index.jsx' // eslint-disable-line
+import App from './views/index.jsx' // eslint-disable-line
 
 const render = (Component) => { ReactDOM.render(<Component />, document.getElementById('app')) } // eslint-disable-line
 render(App);
