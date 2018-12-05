a921d2ac4b432e2d5099a42b385e66f68ebbdaec
孙亮 <sunliang@qudian.com>
2018-12-05 09:47:07 +0800

    fix:eslint

--- src/index-ssr.js
+++ src/index-ssr.js
@@ -1,4 +1,4 @@
-import React from 'react'
-import App from './views/index' // eslint-disable-line
+import React from 'react';
+import App from './views/index';
 
-export default <App /> // eslint-disable-line
+export default <App />;
